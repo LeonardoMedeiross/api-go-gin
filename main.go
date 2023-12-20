6465d9a6dd6e53d4c6e61f3501df1dc0dba6b24b
Leonardo Medeiros <leo-sbs@live.com>
2023-12-20 12:24:52 -0300

    add, exclusão e outras formas de procurar o aluno

--- main.go
+++ main.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-	"github.com/LeonardoMedeiross/api-go-gin/models"
+	"github.com/LeonardoMedeiross/api-go-gin/database"
 	"github.com/LeonardoMedeiross/api-go-gin/routes"
 )
 
 func main() {
-	models.Alunos = []models.Aluno{
-		{Nome: "Leonardo", CPF: "1234567850", RG: "36925814"},
-		{Nome: "Crystiane", CPF: "00000000000", RG: "37925814"},
-	}
+	database.ConectaComBancoDeDados()
 	routes.HandleRequest()
 }
--- routes/routes.go
+++ routes/routes.go
@@ -9,5 +9,11 @@ func HandleRequest() {
 	r := gin.Default()
 	r.GET("/alunos", controllers.ExibeTodosOsAlunos)
 	r.GET("/:nome", controllers.Saudacao)
+	r.POST("/alunos", controllers.CriaNovoAluno)
+	r.GET("/alunos/:id", controllers.BuscaAlunoPorId)
+	r.DELETE("/alunos/:id", controllers.DeletaAluno)
+	r.PATCH("/alunos/:id", controllers.EditarAluno)
+	r.GET("/alunos/cpf/:cpf", controllers.BuscaAlunoPorCpf)
+	r.GET("/alunos/rg/:rg", controllers.BuscaAlunoPorRg)
 	r.Run()
 }
